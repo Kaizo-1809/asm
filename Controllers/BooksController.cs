233abaf582e3ea30b4d3c0d64d3c974cc9b6f3c5
Kaizo-1809 <namkhanhnguyen770@gmail.com>
2024-01-08 02:32:03 +0700

    up

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -100,7 +100,6 @@ namespace asm.Controllers
             return View(bookEdit);
         }
 
-        // POST: ProductsAdmin/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
